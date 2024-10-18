ba6e3c617f9b75b961c24ddd7585f67de7466b14
AnuradhaManda116 <manda.anuradha@ltimindtree.com>
2024-10-18 17:28:09 +0530

    latest files

--- src/main/java/org/example/controller/WeatherController.java
+++ src/main/java/org/example/controller/WeatherController.java
@@ -3,9 +3,12 @@ package org.example.controller;
 import org.example.model.WeatherData;
 import org.example.service.WeatherService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,6 +24,12 @@ public class WeatherController {
 	 @GetMapping("/{city}")
 	 public WeatherData getWeather(@PathVariable String city) throws JsonProcessingException{
 		 return this.weatherService.getWeatherOfCity(city);
+	 }
+	 
+	 @PostMapping("/add")
+	 public ResponseEntity<WeatherData> getWeathertwo(@RequestBody String city) throws JsonProcessingException {
+	    	WeatherData weatherData = weatherService.getWeatherOfCity(city);
+	        return ResponseEntity.ok(weatherData);
 	 }
 	 
      @DeleteMapping("/{id}")
